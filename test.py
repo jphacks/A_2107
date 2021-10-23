7e41420c5f71e72417bfc0d437b9428b2362f66a
sanji0219 <81353953+sanji0219@users.noreply.github.com>
2021-10-23 13:40:40 +0900

    m

--- test.py
+++
@@ -1,7 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
-def firebase():
-    cred = credentials.Certificate('nokhik-firebase-adminsdk-furbv-6ea3aa26e4.json')
-    firebase_admin.initialize_app(cred)
